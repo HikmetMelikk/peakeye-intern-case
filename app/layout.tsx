1329bd4d487c918a0baf842f42c64e77a399d7f1
Hikmet Melik Fırat <hikmetmelik@gmail.com>
2024-12-22 15:31:01 +0300

    fix: Update title in layout metadata to reflect new branding as PeakEye Blog Website

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-	title: "PeakEye Case",
+	title: "PeakEye Blog Website",
 	description: "PeakEye Internship Case Project",
 };
 
